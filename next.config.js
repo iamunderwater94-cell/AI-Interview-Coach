f26b44a33b02fbb6f879b95338dc3108f798b3d7
Aditi Kamath A <aditikamath06@gmail.com>
2026-08-05 13:14:13 +0530

    Implement enterprise reverse proxy for Firebase Auth to completely bypass all 3rd party cookie blockers

--- next.config.js
+++ next.config.js
@@ -9,6 +9,14 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination: 'https://ai-interview-coach-22ab0.firebaseapp.com/__/auth/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
